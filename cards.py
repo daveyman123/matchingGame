08450ab25f2636f735cd0a898098d646018c3e33
David Graff <djgraff1@cougars.ccis.edu>
2019-02-05 16:04:05 -0600

    refined some OOP stuff

--- cards.py
+++ cards.py
@@ -1,16 +1,15 @@
 import random
 
-
-
-
 class Card(object):
     def __init__(self,suit,val,mana,health, attack):
+
         self.suit = suit
         self.value = val
         self.mana = mana
 
         self.health = health
         self.attack = attack
+
 
     def getHealth(self):
         return self.health
@@ -24,20 +23,19 @@ class Card(object):
     def setAttack(self, attack):
         self.attack = attack
 
-    def getCards(self):
-        return self
-
-
-
-
-
     def show(self):
         print "{} of {} with {} mana {} health {} attack".format(self.value,self.suit,self.mana,self.health,self.attack)
+
+
+
 
 class Deck(object):
     def __init__(self):
         self.cards = {}
+
+
         self.build()
+
     def build(self):
         for s in ("S", "C", "D", "H"):
             for v in range (1,14):
@@ -66,8 +64,12 @@ class Deck(object):
                     attack = 5
                     health = 5
 
+
+
                 self.cards[s + str(v)]=Card(s, v, mana,health,attack)
 
+    def getCards(self):
+        return self.cards
 
 
     def show(self):
@@ -88,17 +90,22 @@ class Deck(object):
 #            r = random.randint(0,i)
 #            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
 
-    def drawCard(self):
-        d = {}
-        randomChoice = random.choice(self.cards.keys())
-        value = self.cards[randomChoice]
-        self.cards.pop(randomChoice)
 
-        d = {randomChoice:value}
-        return d
-class Table(object):
+
+class Hand(object):
     def __init__(self):
-        self.cards = {}
+        self.hand = {}
+
+class Board(object):
+    def __init__(self):
+        self.mpzCards = {}
+
+
+        self.deck = Deck()
+
+    def minionPlayZone1(self):
+        return self.mpz1Cards
+
 
     def removeCards(self, cards):
 
@@ -120,15 +127,20 @@ class Table(object):
         for k,v in self.cards.iteritems():
             v.show()
 
+    # def combine(self):
+    #     return self.player1.getTable().getCards().update(self.player2.getTable().getCards())
 
-
-class Player(object):
+class Player(Board):
     def __init__(self, name, hero):
-        self.hand = {}
+        super(Player, self).__init__()
         self.mana = 0
         self.name = name
         self.hero = hero
-        self.table = Table()
+
+        self.turn = 0
+        self.card = {}
+        self.hand = {}
+        self.cardToBePlayed = {}
 
     def getMana(self):
         return self.mana
@@ -137,19 +149,20 @@ class Player(object):
         return self.table
 
     def playCard(self, card):
+        input = raw_input("enter a card")
         d = {}
         if card in self.hand:
             cardToBePlayed = self.hand[card]
 
             self.hand.pop(card)
-            d = {card:cardToBePlayed}
-            self.table.addCard(d)
+            self.cardToBePlayed = {card:cardToBePlayed}
+            self.mpzCards.addCard(d)
 
         return d
 
-    def draw(self, deck):
-        self.hand.update(deck.drawCard())
-        return self
+    # def draw(self, deck):
+    #     self.hand.update(deck.drawCard())
+    #     return self
     def showHand(self):
         for key,value in self.hand.iteritems():
             value.show()
@@ -164,60 +177,51 @@ class Player(object):
     def showInfo(self):
         print "hero = {} mana = {}".format(self.hero,self.mana)
 
+    def drawCard(self):
 
-    def attack(self,card1, card2):
-        card1Attack = (card1.getHealth() - card2.getAttack())
-        card2Attack = (card2.getHealth() - card1.getAttack())
-        card1.setHealth(card1Attack)
-        card2.setHealth(card2Attack)
-        h = {}
-        d = self.table.getCards()
-        for k,v in d.iteritems():
-            if v.getHealth()<=1:
-                h[k]=v
-        self.table.removeCards(h)
+        randomChoice = random.choice(self.deck.cards.keys())
+        value = self.deck.cards[randomChoice]
+        self.deck.cards.pop(randomChoice)
+
+        self.card = {randomChoice:value}
+        self.hand.update(self.card)
 
 
 
-deck = Deck()
-
-deck.shuffle2()
-
-bob = Player("bob", "Shaman")
-bilbo = Player("bilbo", "Warlock")
-
-bob.draw(deck).draw(deck).draw(deck)
-bob.showInfo()
-print "bob hand"
-bob.showHand()
-try:
-    card1 = str(raw_input("pick a card to play for example: S8 = 8 of spades "))
-except:
-    print"error"
-
-bob.playCard(card1)
-print "bob hand"
-bob.showHand()
 
 
-##Bilbo setup
-bilbo.draw(deck).draw(deck).draw(deck)
-bilbo.showInfo()
-print "bilbos hand"
-bilbo.showHand()
+        # card2.getPlayer2().getTable().removeCards(h)
 
-card2 = random.choice(bilbo.getHand().keys())
-bilbo.playCard(card2)
-print "bilbos table"
-bilbo.getTable().show()
+    def playerInfo(self):
+        print "cards in player {}'s hand, mana left {}".format(self.name,self.mana)
 
-print "bob table"
-bob.getTable().show()
 
-print "cards are attacking"
-bob.attack(bob.getTable().getCard(card1),bilbo.getTable().getCard(card2))
-print "bilbos table"
-bilbo.getTable().show()
 
-print "bob table"
-bob.getTable().show()
+# class TurnPhases():
+#     def __init__(self,player1,player2,table,deck):
+
+class Game(object):
+
+
+    def run(self):
+        gameOver = False
+        deck = Deck()
+        player1 = Player("bob", "warlock")
+        player2 = Player("bilbo", "shaman")
+        while gameOver == False:
+            player1.mana += 1
+            player1.showInfo()
+            player1.drawCard()
+            player1.playerInfo()
+            player1.showHand()
+            gameOver = True
+
+
+
+
+def main():
+    game = Game()
+    game.run()
+
+if __name__ == '__main__':
+    main()
